1d83e92b481aeaeaf2ac86b14731775fef3bee41
Ketan Kunkalikar <ketankunkalikar33@gmail.com>
2025-07-20 11:31:53 +0530

    fix: outdated imports while running sagan.py

--- sagan_smolagents/ingest_data/ingest_data.py
+++ sagan_smolagents/ingest_data/ingest_data.py
@@ -4,7 +4,7 @@ from typing import Optional
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import (
+from langchain_community.document_loaders import (
     PyPDFLoader,
     TextLoader,
     CSVLoader,
